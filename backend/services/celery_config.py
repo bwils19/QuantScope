50e7775fcfad81b0337437ac56df63553e8f696b
root <info@prophetanalytics.com>
2025-03-18 17:18:12 +0000

    adding the celery_config to tracking

---
+++ backend/services/celery_config.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+def make_celery(app):
+    """Configure Celery with Flask app."""
+    celery = Celery(
+        app.import_name,
+        broker="redis://localhost:6379/0",
+        backend="redis://localhost:6379/0",
+        include=["backend.services.price_update_service"],
+    )
+    celery.conf.update(app.config)
+    return celery
